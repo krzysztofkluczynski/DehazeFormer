d440a44d2b89bdb1a05b4c8f2ab7e383ce437c82
towienko <01169218@pw.edu.pl>
2025-05-05 20:01:37 +0200

    Log training to wandb

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from torch.cuda.amp import autocast, GradScaler
 from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
 from tqdm import tqdm
+import wandb
 
 from utils import AverageMeter
 from datasets.loader import PairLoader, CityScapesPairLoader
@@ -25,19 +26,26 @@ parser.add_argument('--log_dir', default='./logs/', type=str, help='path to logs
 parser.add_argument('--dataset', default='RESIDE-IN', type=str, help='dataset name')
 parser.add_argument('--config', default='configs/indoor/default.json', type=str, help='path to training config file')
 parser.add_argument('--gpu', default='0,1,2,3', type=str, help='GPUs used for training')
+
+# Wandb options
+parser.add_argument("--enable_wandb", action='store_true', default=False, help="Use Weights & Biases for logging")
+parser.add_argument("--wandb_team", type=str, default=None, help="Weights & Biases team name")
+parser.add_argument("--wandb_project", type=str, default=None, help="Weights & Biases project name")
+parser.add_argument("--wandb_run_name", type=str, default=None, help="Weights & Biases current run name")
+
 args = parser.parse_args()
 
 os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 
 
-def train(train_loader, network, criterion, optimizer, scaler):
+def train(train_loader, network, criterion, optimizer, scaler, epoch, wandb_run=None):
 	losses = AverageMeter()
 
 	torch.cuda.empty_cache()
 	
 	network.train()
 
-	for batch in train_loader:
+	for i, batch in enumerate(tqdm(train_loader, desc=f'Training epoch {epoch}')):
 		source_img = batch['source'].cuda()
 		target_img = batch['target'].cuda()
 
@@ -47,22 +55,29 @@ def train(train_loader, network, criterion, optimizer, scaler):
 
 		losses.update(loss.item())
 
+
 		optimizer.zero_grad()
 		scaler.scale(loss).backward()
 		scaler.step(optimizer)
 		scaler.update()
 
+		if wandb_run is not None:
+			wandb.log({
+				"train_loss": loss.item(),
+				"epoch": epoch
+			}, step=(epoch) * len(train_loader) + i)
+
 	return losses.avg
 
 
-def valid(val_loader, network):
+def valid(val_loader, network, step, wandb_run=None):
 	PSNR = AverageMeter()
 
 	torch.cuda.empty_cache()
 
 	network.eval()
 
-	for batch in val_loader:
+	for i, batch in enumerate(tqdm(val_loader, desc='Validating', leave=False)):
 		source_img = batch['source'].cuda()
 		target_img = batch['target'].cuda()
 
@@ -73,7 +88,43 @@ def valid(val_loader, network):
 		psnr = 10 * torch.log10(1 / mse_loss).mean()
 		PSNR.update(psnr.item(), source_img.size(0))
 
+		if wandb_run is not None:
+			wandb.log({
+				"val_psnr": psnr.item()
+			}, step=step)
+
 	return PSNR.avg
+
+
+def get_wandb_run(args, settings, ckpt):
+	assert args.enable_wandb and args.wandb_project is not None and args.wandb_team is not None, \
+		"get_wandb_run was called, but not all required arguments were provided."
+
+	WANDB_TOKEN = os.getenv("WANDB_TOKEN")
+	assert WANDB_TOKEN, "WANDB_TOKEN environment variable not set. Please set it to your Weights & Biases API key."
+	wandb.login(key=WANDB_TOKEN, verify=True)
+
+	if args.wandb_run_name is None:
+		 wandb_run_name = "_".join([
+			args.model,
+			"ckpt-" + (ckpt or "scratch").split('/')[-1].split('.')[0],
+			f"batch-{settings['batch_size']}",
+			f"lr-{settings['lr']}-{settings['optimizer']}",
+			f"edge-{settings['edge_decay']}",
+			f"patch-{settings['patch_size']}",
+			f"batch-{settings['batch_size']}",
+			f"epochs-{settings['epochs']}",
+		 ])
+	else:
+		 wandb_run_name = args.wandb_run_name
+
+	run = wandb.init(
+		project=args.wandb_project,
+		entity=args.wandb_team,
+		name=wandb_run_name,
+		config=dict(**vars(args), **settings),
+	)
+	return run
 
 
 if __name__ == '__main__':
@@ -147,6 +198,11 @@ if __name__ == '__main__':
 	save_dir = os.path.join(args.save_dir, exp_name)
 	os.makedirs(save_dir, exist_ok=True)
 
+	if args.enable_wandb:
+		wandb_run = get_wandb_run(args, setting, args.ckpt)
+	else:
+		wandb_run = None
+
 	if not args.ckpt:
 		print('==> Start training, current model name: ' + args.model)
 		# print(network)
@@ -154,15 +210,15 @@ if __name__ == '__main__':
 		writer = SummaryWriter(log_dir=os.path.join(args.log_dir, exp_name, args.model))
 
 		best_psnr = 0
-		for epoch in tqdm(range(setting['epochs'] + 1)):
-			loss = train(train_loader, network, criterion, optimizer, scaler)
+		for epoch in tqdm(range(setting['epochs'] + 1), desc=f"Training for {setting['epochs']} epochs", leave=False):
+			loss = train(train_loader, network, criterion, optimizer, scaler, epoch=epoch, wandb_run=wandb_run)
 
 			writer.add_scalar('train_loss', loss, epoch)
 
 			scheduler.step()
 
 			if epoch % setting['eval_freq'] == 0:
-				avg_psnr = valid(val_loader, network)
+				avg_psnr = valid(val_loader, network, step=(epoch + 1) * len(train_loader) - 1, wandb_run=wandb_run)
 				
 				writer.add_scalar('valid_psnr', avg_psnr, epoch)
 
